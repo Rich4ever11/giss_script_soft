e19fde08e78037f13fa231ca502ff8ef80db819d
Rich4ever11 <rich4ever11@protonmail.com>
2024-09-13 20:35:25 -0400

    limit flask access to the server

--- scripts/route/api.py
+++ scripts/route/api.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from netCDF4 import Dataset
 import numpy as np
@@ -8,7 +9,7 @@ import json
 from json import JSONEncoder
 import xarray
 import json
-from flask import request
+from flask import request, abort
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
@@ -21,6 +22,13 @@ GEO_DATA_SCRIPTS_FOLDER = "../geo_scripts"
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.before_request
+def limit_remote_addr():
+    remote_ip = str(request.remote_addr)
+    if remote_ip != "127.0.0.1":
+        abort(403)
 
 
 VALID_SCRIPTS = [
@@ -48,9 +56,48 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/")
+def get_geofile_data(file_path):
+    var_info_list = []
+    with Dataset(file_path) as netcdf_dataset:
+        for var in netcdf_dataset.variables:
+            if len(netcdf_dataset.variables[var].shape) > 1:
+                var_data = {
+                    "name": var,
+                    "dimensions": netcdf_dataset.variables[var].shape,
+                }
+                var_info_list.append(var_data)
+    geo_data = {"variables": var_info_list}
+    return geo_data
+
+
+def obtain_file_metadata(file_path):
+    metadata = {}
+    with open(file_path) as f:
+        for line in f:
+            if line.startswith("#"):
+                # Obtain the key value comment data
+                line = line.replace("#", "")
+                line = line.strip()
+                key, value = str(line).split(":")
+                metadata[key.strip()] = value.strip()
+            else:
+                # Here you can proceed lines that are not comments
+                return metadata
+    return metadata
+
+
+@app.route("/view_scripts", methods=["GET"])
 def obtain_geoscripts():
-    return json.dumps(VALID_SCRIPTS)
+    file_data = []
+    for file in VALID_SCRIPTS:
+        file_data.append({file: obtain_file_metadata(file)})
+    return json.dumps(file_data)
+
+
+@app.route("/view_geofile_data", methods=["GET"])
+def obtain_geofile_data():
+    if request.method == "GET":
+        return get_geofile_data(VALID_GEO_FILES[0])
 
 
 @app.route("/send_netcdf", methods=["GET", "POST"])
@@ -60,7 +107,6 @@ def obtain_netcdf_data():
         # check if the post request has the file part
         if "file" not in files_dict:
             print("No file part")
-
         file = files_dict["files"]
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
@@ -87,6 +133,6 @@ def run_geoscript():
             return json.dumps({"error": "invalid file passed in"})
 
 
-@app.route("/obtain_geo_files")
-def obtain_geodata():
-    return json.dumps(VALID_GEO_FILES)
+# @app.route("/obtain_geo_file")
+# def obtain_geodata():
+#     return json.dumps(VALID_GEO_FILES)
